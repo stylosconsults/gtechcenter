e129e5a27b9961fe35ba540851cb9d5e6de86d32
CK-EmbPro <kennydebrice2@gmail.com>
2024-09-17 18:35:18 +0200

    layout page modified

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import {Open_Sans} from "next/font/google"
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 
 const open_sans = Open_Sans({
@@ -26,6 +27,7 @@ export default function RootLayout({
       <body>
         <Navbar />
         {children}
+        <Footer/>
       </body>
     </html>
   );
