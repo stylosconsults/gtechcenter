05b1b4716f267a7c1c236a1363125ba09f47d3a6
CK-EmbPro <kennydebrice2@gmail.com>
2024-09-19 15:35:25 +0200

    google maps added added

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,12 +1,24 @@
+"use client"
 import PagesTopDiv from '@/components/PagesTopDiv'
 import React from 'react'
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
+
+const containerStyle = {
+    width: "100%",
+    height: "100%"
+}
+
+const center = {
+    lat: -1.9757035631385522,
+    lng: 30.10670646256887
+}
 
 const page = () => {
     return (
         <div className='flex flex-col'>
             <PagesTopDiv heading='Contact Us' paragraph='Home Contact' />
 
-            <div className='flex h-[500px]'>
+            <div className='flex h-[500px] bg-headerBgColor'>
 
                 <div className='flex flex-col gap-3 pt-2 w-[50%] h-full px-4 justify-center'>
                     <p className='text-textColor text-[2em] font-semibold h-[10%]'>Contact For Any Queries</p>
@@ -24,7 +36,22 @@ const page = () => {
 
                 </div>
 
-                <div className='w-[50%] flex justify-center items-center '>Google map</div>
+                <div className='w-[50%] flex justify-center items-center'>
+                    {process.env.NEXT_PUBLIC_GOOGLE_MAPS_API ?
+                        (<LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API}>
+                            <GoogleMap 
+                                mapContainerStyle={containerStyle}
+                                center={center}
+                                zoom={19}
+                            >
+                                <Marker position={center}/>
+                            </GoogleMap>
+                        </LoadScript>)
+                        :
+                        <p>Google map api is missing</p>
+
+                    }
+                </div>
             </div>
 
         </div>
