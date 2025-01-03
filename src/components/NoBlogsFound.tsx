66c36da3cb55420ad2b8a35d73866c310707fd0e
CK-EmbPro <kennydebrice2@gmail.com>
2025-01-04 01:13:34 +0200

    modifications made

--- src/components/NoBlogsFound.tsx
+++ src/components/NoBlogsFound.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 const NoBlogsFound = () => {
     return (
-        <div className='col-span-1 md:col-span-2 lg-col-span-3  h-[30vh] flex justify-center items-center text-[2em] '>
-            <File className='w-[30%] h-full ' size={48} color="gray" />
+        <div className='col-span-1 md:col-span-2 lg:col-span-3 h-[10vh] md:h-[30vh] flex justify-center items-center text-[2em] '>
+            <File className='w-[20%] h-full ' size={48} color="gray" />
 
-            <p className='text-center text-[3vw]'>No blogs found </p>
+            <p className='text-center text-[5vw] md:text-[2.2vw]'>No blogs found </p>
         </div>
     )
 }
