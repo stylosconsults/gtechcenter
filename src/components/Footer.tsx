5cbb395518e8a17e223c64e3d967a86398a1bf7e
nkurunziza1 <alexendre.nkurunziza@gmail.com>
2025-08-01 11:12:17 +0200

    change address

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,17 @@
-"use client"
-import Link from 'next/link'
-import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react'
-import Facebook from "../../public/icons/facebook.svg"
-import Instagram from "../../public/icons/instagram.svg"
-import Twitter from "../../public/icons/twitter.svg"
-import LinkedIn from "../../public/icons/linkedIn.svg"
-import { useSubscription } from '@/hooks/useSubscription'
+"use client";
+import Link from "next/link";
+import React, { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import Facebook from "../../public/icons/facebook.svg";
+import Instagram from "../../public/icons/instagram.svg";
+import Twitter from "../../public/icons/twitter.svg";
+import LinkedIn from "../../public/icons/linkedIn.svg";
+import { useSubscription } from "@/hooks/useSubscription";
 
 // const Footer = () => {
 //     const {  loading, createSubscription, subscribeSuccessMsgs } = useSubscription()
 //     const [subscribingEmail, setSubscribingEmail] = useState<string>("")
 //     const [disableSubscribeBtn, setDisableSubscribeBtn] = useState<boolean>(false)
-    
+
 //     const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
 //         const email = e.target.value
 //         setSubscribingEmail(email)
@@ -23,13 +23,11 @@ import { useSubscription } from '@/hooks/useSubscription'
 
 //     }
 
-
 //     useEffect(()=>{
 //         if(subscribeSuccessMsgs.createSuccessMsg !==""){
 //             setSubscribingEmail("")
 //         }
 //     }, [subscribeSuccessMsgs.createSuccessMsg])
-
 
 //     useEffect(() => {
 
@@ -39,9 +37,6 @@ import { useSubscription } from '@/hooks/useSubscription'
 //             setDisableSubscribeBtn(false)
 //         }
 //     }, [loading])
-
-
-
 
 //     return (
 //         <div className='flex flex-col bg-textColor'>
@@ -61,7 +56,7 @@ import { useSubscription } from '@/hooks/useSubscription'
 //                         {!loading ?
 //                         "Sign Up":
 //                         "Loading"
-                        
+
 //                         }
 //                     </button>
 //                 </form>
@@ -91,9 +86,9 @@ import { useSubscription } from '@/hooks/useSubscription'
 //                 <div className='flex flex-col gap-3'>
 //                     <p className='text-white font-semibold text-[1.1em]'>Get In Touch</p>
 //                     <div className='flex flex-col gap-1 ps-4 text-headerLinkBorderColor'>
-//                         <Link href={""}>Kk 345 st., Kicukiro Saint Joseph</Link>
+//                         <Link href={""}>KK 317 st., Kicukiro Niboyi</Link>
 //                         <Link href={""}>info@example.com</Link>
-//                         <Link href={""}>+250788647871</Link>
+//                         <Link href={""}>+250 787 919 252</Link>
 //                     </div>
 //                 </div>
 //                 <div className='flex flex-col gap-3'>
@@ -117,113 +112,198 @@ import { useSubscription } from '@/hooks/useSubscription'
 
 // export default Footer
 
-
-
 const Footer = () => {
-    const { loading, createSubscription, subscribeSuccessMsgs } = useSubscription()
-    const [subscribingEmail, setSubscribingEmail] = useState<string>("")
-    const [disableSubscribeBtn, setDisableSubscribeBtn] = useState<boolean>(false)
-    
-    const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const email = e.target.value
-        setSubscribingEmail(email)
+  const { loading, createSubscription, subscribeSuccessMsgs } =
+    useSubscription();
+  const [subscribingEmail, setSubscribingEmail] = useState<string>("");
+  const [disableSubscribeBtn, setDisableSubscribeBtn] =
+    useState<boolean>(false);
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const email = e.target.value;
+    setSubscribingEmail(email);
+  };
+
+  const handleOnSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    await createSubscription(subscribingEmail);
+  };
+
+  useEffect(() => {
+    if (subscribeSuccessMsgs.createSuccessMsg !== "") {
+      setSubscribingEmail("");
     }
+  }, [subscribeSuccessMsgs.createSuccessMsg]);
 
-    const handleOnSubmit = async (e: FormEvent) => {
-        e.preventDefault()
-        await createSubscription(subscribingEmail)
-    }
+  useEffect(() => {
+    setDisableSubscribeBtn(loading);
+  }, [loading]);
 
-    useEffect(() => {
-        if(subscribeSuccessMsgs.createSuccessMsg !== "") {
-            setSubscribingEmail("")
-        }
-    }, [subscribeSuccessMsgs.createSuccessMsg])
+  return (
+    <div className="flex flex-col bg-textColor">
+      {/* Subscribe Section */}
+      <div className="flex flex-col gap-4 md:gap-6 py-6 md:py-[30px] items-center bg-headerInfoBgColor px-4 md:px-0">
+        <p className="text-textColor font-semibold text-2xl md:text-[2.5em] text-center">
+          Stay Update!!!
+        </p>
+        <form
+          onSubmit={handleOnSubmit}
+          className="flex w-full md:w-[50%] lg:w-[30%] rounded-[5px] overflow-hidden justify-between"
+        >
+          <input
+            type="text"
+            placeholder="Your Email"
+            value={subscribingEmail}
+            onChange={handleEmailChange}
+            className="w-[70%] md:w-[80%] outline-none p-2 md:p-3"
+            required
+          />
+          <button
+            disabled={disableSubscribeBtn || loading}
+            className={`${
+              loading || disableSubscribeBtn ? "bg-slate-700" : "bg-textColor"
+            } text-white w-[30%] md:w-[20%] p-2 text-sm md:text-base`}
+          >
+            {!loading ? "Sign Up" : "Loading"}
+          </button>
+        </form>
+      </div>
 
-    useEffect(() => {
-        setDisableSubscribeBtn(loading)
-    }, [loading])
-
-    return (
-        <div className='flex flex-col bg-textColor'>
-            {/* Subscribe Section */}
-            <div className='flex flex-col gap-4 md:gap-6 py-6 md:py-[30px] items-center bg-headerInfoBgColor px-4 md:px-0'>
-                <p className='text-textColor font-semibold text-2xl md:text-[2.5em] text-center'>Stay Update!!!</p>
-                <form onSubmit={handleOnSubmit} className='flex w-full md:w-[50%] lg:w-[30%] rounded-[5px] overflow-hidden justify-between'>
-                    <input
-                        type="text"
-                        placeholder='Your Email'
-                        value={subscribingEmail}
-                        onChange={handleEmailChange}
-                        className='w-[70%] md:w-[80%] outline-none p-2 md:p-3'
-                        required
-                    />
-                    <button 
-                        disabled={disableSubscribeBtn || loading} 
-                        className={`${loading || disableSubscribeBtn ? "bg-slate-700" : "bg-textColor"} text-white w-[30%] md:w-[20%] p-2 text-sm md:text-base`}
-                    >
-                        {!loading ? "Sign Up" : "Loading"}
-                    </button>
-                </form>
-            </div>
-
-            {/* Links Section */}
-            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 px-6 md:px-10 lg:px-20 my-8 md:my-[2em]'>
-                {/* Quick Links */}
-                <div className='flex flex-col gap-3'>
-                    <p className='text-white font-semibold text-[1.1em]'>Quick Links</p>
-                    <div className='flex flex-col gap-1 ps-4 text-headerLinkBorderColor'>
-                        <Link href="/" className='hover:text-white transition-colors'>Home</Link>
-                        <Link href="/about" className='hover:text-white transition-colors'>About Us</Link>
-                        <Link href="/services" className='hover:text-white transition-colors'>Our Services</Link>
-                        <Link href="/blogs" className='hover:text-white transition-colors'>Latest Blog Post</Link>
-                        <Link href="/contact" className='hover:text-white transition-colors'>Contact Us</Link>
-                    </div>
-                </div>
-
-                {/* Popular Links */}
-                <div className='flex flex-col gap-3'>
-                    <p className='text-white font-semibold text-[1.1em]'>Popular Links</p>
-                    <div className='flex flex-col gap-1 ps-4 text-headerLinkBorderColor'>
-                        <Link href="/" className='hover:text-white transition-colors'>Home</Link>
-                        <Link href="/about" className='hover:text-white transition-colors'>About Us</Link>
-                        <Link href="/services" className='hover:text-white transition-colors'>Our Services</Link>
-                        <Link href="/blogs" className='hover:text-white transition-colors'>Latest Blog Post</Link>
-                        <Link href="/contact" className='hover:text-white transition-colors'>Contact Us</Link>
-                    </div>
-                </div>
-
-                {/* Get In Touch */}
-                <div className='flex flex-col gap-3'>
-                    <p className='text-white font-semibold text-[1.1em]'>Get In Touch</p>
-                    <div className='flex flex-col gap-1 ps-4 text-headerLinkBorderColor'>
-                        <Link href="" className='hover:text-white transition-colors'>Kk 345 st., Kicukiro Saint Joseph</Link>
-                        <a href="mailto:gwissenconsult@gmail.com" className='hover:text-white transition-colors'>gwissenconsult@gmail.com</a>
-                        <a href="tel:+50788647871" className='hover:text-white transition-colors'>+250788647871</a>
-                    </div>
-                </div>
-
-                {/* Follow Us */}
-                <div className='flex flex-col gap-3'>
-                    <p className='text-white font-semibold text-[1.1em]'>Follow Us</p>
-                    <div className='flex gap-3 ps-4'>
-                        <Link target='_blank' rel='noopener noreferrer' href="https://x.com/wissen_g14898" className='hover:opacity-80 transition-opacity'><Twitter /></Link>
-                        <Link target='_blank' rel='noopener noreferrer' href="https://www.facebook.com/" className='hover:opacity-80 transition-opacity'><Facebook /></Link>
-                        <Link target='_blank' rel='noopener noreferrer' href="https://www.linkedin.com/notifications/" className='hover:opacity-80 transition-opacity'><LinkedIn /></Link>
-                        <Link target='_blank' rel='noopener noreferrer' href="https://www.instagram.com/" className='hover:opacity-80 transition-opacity'><Instagram /></Link>
-                    </div>
-                </div>
-            </div>
-
-            {/* Copyright Section */}
-            <div className='my-3 px-4 md:px-0'>
-                <p className='text-center text-headerLinkBorderColor text-sm md:text-base'>
-                    © <span className='underline underline-offset-[5px] inline-block hover:text-white transition-colors'>G-Wissen</span>. 
-                    All Rights Reserved. Designed by <span className='underline underline-offset-[5px] inline-block hover:text-white transition-colors'>g-wissen</span>
-                </p>
-            </div>
+      {/* Links Section */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 px-6 md:px-10 lg:px-20 my-8 md:my-[2em]">
+        {/* Quick Links */}
+        <div className="flex flex-col gap-3">
+          <p className="text-white font-semibold text-[1.1em]">Quick Links</p>
+          <div className="flex flex-col gap-1 ps-4 text-headerLinkBorderColor">
+            <Link href="/" className="hover:text-white transition-colors">
+              Home
+            </Link>
+            <Link href="/about" className="hover:text-white transition-colors">
+              About Us
+            </Link>
+            <Link
+              href="/services"
+              className="hover:text-white transition-colors"
+            >
+              Our Services
+            </Link>
+            <Link href="/blogs" className="hover:text-white transition-colors">
+              Latest Blog Post
+            </Link>
+            <Link
+              href="/contact"
+              className="hover:text-white transition-colors"
+            >
+              Contact Us
+            </Link>
+          </div>
         </div>
-    )
-}
 
-export default Footer
+        {/* Popular Links */}
+        <div className="flex flex-col gap-3">
+          <p className="text-white font-semibold text-[1.1em]">Popular Links</p>
+          <div className="flex flex-col gap-1 ps-4 text-headerLinkBorderColor">
+            <Link href="/" className="hover:text-white transition-colors">
+              Home
+            </Link>
+            <Link href="/about" className="hover:text-white transition-colors">
+              About Us
+            </Link>
+            <Link
+              href="/services"
+              className="hover:text-white transition-colors"
+            >
+              Our Services
+            </Link>
+            <Link href="/blogs" className="hover:text-white transition-colors">
+              Latest Blog Post
+            </Link>
+            <Link
+              href="/contact"
+              className="hover:text-white transition-colors"
+            >
+              Contact Us
+            </Link>
+          </div>
+        </div>
+
+        {/* Get In Touch */}
+        <div className="flex flex-col gap-3">
+          <p className="text-white font-semibold text-[1.1em]">Get In Touch</p>
+          <div className="flex flex-col gap-1 ps-4 text-headerLinkBorderColor">
+            <Link href="" className="hover:text-white transition-colors">
+              KK 317 st., Kicukiro Niboyi
+            </Link>
+            <a
+              href="mailto:gwissenconsult@gmail.com"
+              className="hover:text-white transition-colors"
+            >
+              gwissenconsult@gmail.com
+            </a>
+            <a
+              href="tel:+50788647871"
+              className="hover:text-white transition-colors"
+            >
+              +250 787 919 252
+            </a>
+          </div>
+        </div>
+
+        {/* Follow Us */}
+        <div className="flex flex-col gap-3">
+          <p className="text-white font-semibold text-[1.1em]">Follow Us</p>
+          <div className="flex gap-3 ps-4">
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://x.com/wissen_g14898"
+              className="hover:opacity-80 transition-opacity"
+            >
+              <Twitter />
+            </Link>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.facebook.com/"
+              className="hover:opacity-80 transition-opacity"
+            >
+              <Facebook />
+            </Link>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.linkedin.com/notifications/"
+              className="hover:opacity-80 transition-opacity"
+            >
+              <LinkedIn />
+            </Link>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.instagram.com/"
+              className="hover:opacity-80 transition-opacity"
+            >
+              <Instagram />
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      {/* Copyright Section */}
+      <div className="my-3 px-4 md:px-0">
+        <p className="text-center text-headerLinkBorderColor text-sm md:text-base">
+          ©{" "}
+          <span className="underline underline-offset-[5px] inline-block hover:text-white transition-colors">
+            G-Wissen
+          </span>
+          . All Rights Reserved. Designed by{" "}
+          <span className="underline underline-offset-[5px] inline-block hover:text-white transition-colors">
+            g-wissen
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,52 +1,62 @@
-"use client"
-import React from 'react'
-import { Open_Sans } from 'next/font/google';
-import Link from 'next/link';
-import InfoIcon from '../../public/icons/infoIcon.svg'
-import PhoneIcon from "../../public/icons/phoneIcon.svg"
-
+"use client";
+import React from "react";
+import { Open_Sans } from "next/font/google";
+import Link from "next/link";
+import InfoIcon from "../../public/icons/infoIcon.svg";
+import PhoneIcon from "../../public/icons/phoneIcon.svg";
 
 const open_sans = Open_Sans({
-    display: "swap",
-    subsets: ['latin'],
-    variable: "--font-open-sans",
-    weight: ["300", "400", "500", "600", "700", "800"],
+  display: "swap",
+  subsets: ["latin"],
+  variable: "--font-open-sans",
+  weight: ["300", "400", "500", "600", "700", "800"],
 });
 
-
-
 const Header = () => {
-
-    return (
-        <div className={`${open_sans.variable}`}>
-            {/* Desktop View */}
-            <div className="hidden md:flex justify-between items-center bg-headerBgColor h-[35px]">
-                <div className="flex items-center ps-[35px] w-[40%]">
-                    <Link className="border-r-headerLinkBorderColor border-r-2 flex justify-center w-[30%] items-center h-full text-welcomeBgColor" href="/frequently_asked">FAQs</Link>
-                    <Link className="border-r-headerLinkBorderColor border-r-2 flex justify-center w-[30%] items-center h-full text-welcomeBgColor" href="/contact">Support</Link>
-                    <Link className="flex justify-center w-[30%] h-full items-center text-welcomeBgColor" href="/privacy_policy">Privacy Policy</Link>
-                </div>
-
-                <div className="flex items-center ps-9 h-full justify-center pe-[2%] gap-[10%] text-white w-[40%] bg-headerInfoBgColor inclined-left">
-                    <a
-                        href="mailto:gwissenconsult@gmail.com"
-                        className="flex text-[1.3vw] items-center gap-1 font-semibold"
-
-                    >
-
-                        <InfoIcon /> gwissenconsult@gmail.com
-                    </a>
-
-
-                    <a href="tel:+250788647871" className="flex  text-[1.3vw] items-center gap-2 font-semibold">
-                        <PhoneIcon />+250788647871
-                    </a>
-                </div>
-            </div>
-
-          
+  return (
+    <div className={`${open_sans.variable}`}>
+      {/* Desktop View */}
+      <div className="hidden md:flex justify-between items-center bg-headerBgColor h-[35px]">
+        <div className="flex items-center ps-[35px] w-[40%]">
+          <Link
+            className="border-r-headerLinkBorderColor border-r-2 flex justify-center w-[30%] items-center h-full text-welcomeBgColor"
+            href="/frequently_asked"
+          >
+            FAQs
+          </Link>
+          <Link
+            className="border-r-headerLinkBorderColor border-r-2 flex justify-center w-[30%] items-center h-full text-welcomeBgColor"
+            href="/contact"
+          >
+            Support
+          </Link>
+          <Link
+            className="flex justify-center w-[30%] h-full items-center text-welcomeBgColor"
+            href="/privacy_policy"
+          >
+            Privacy Policy
+          </Link>
         </div>
-    );
+
+        <div className="flex items-center ps-9 h-full justify-center pe-[2%] gap-[10%] text-white w-[40%] bg-headerInfoBgColor inclined-left">
+          <a
+            href="mailto:gwissenconsult@gmail.com"
+            className="flex text-[1.3vw] items-center gap-1 font-semibold"
+          >
+            <InfoIcon /> gwissenconsult@gmail.com
+          </a>
+
+          <a
+            href="tel:+250 787 919 252"
+            className="flex  text-[1.3vw] items-center gap-2 font-semibold"
+          >
+            <PhoneIcon />
+            +250 787 919 252
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
